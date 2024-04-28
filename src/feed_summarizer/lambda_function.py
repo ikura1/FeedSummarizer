95a0d652b8a634edd4f5e308c707fa6933a8bdd5
ikura1 <ikura.lab@gmail.com>
2024-04-28 18:43:53 +0900

    feat: 実行日時取得関数を変更 - 引数や存在しない場合など

--- src/feed_summarizer/lambda_function.py
+++ src/feed_summarizer/lambda_function.py
@@ -1,16 +1,16 @@
-from datetime import datetime, timezone
 import json
+from datetime import datetime, timezone
 
 import boto3
 
+from .consts import BUCKET_NAME, OBJECT_KEY
+
 s3 = boto3.client("s3")
-BUCKET_NAME = "FeedSummarizer"
-OBJECT_KEY = "last_run_time.json"
 
 
 def lambda_handler(_event, _context):
     # 前回の実行時間をS3から取得
-    last_run_time = get_last_run_time()
+    last_run_time = get_last_run_time(s3, BUCKET_NAME, OBJECT_KEY)
     # post_feed(datetime.fromisoformat(last_run_time))
 
     # 現在の実行時間を保存
@@ -28,14 +28,24 @@ def lambda_handler(_event, _context):
     }
 
 
-def get_last_run_time():
+def get_last_run_time(
+    s3_client: boto3.Session, bucket_name: str, object_key: str
+) -> datetime:
+    """S3バケットから前回の実行時間を取得する
+
+    Args:
+        s3_client (boto3.Session): S3クライアント
+        bucket_name (str): S3バケット名
+        object_key (str): S3オブジェクトキー
+
+    Returns:
+        datetime: 前回の実行時間
+    """
     try:
-        response = s3.get_object(Bucket=BUCKET_NAME, Key=OBJECT_KEY)
+        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
         last_run_time = json.loads(response["Body"].read().decode("utf-8"))[
             "last_run_time"
         ]
-
-    except Exception as e:
-        last_run_time = None
-        print(f"Error retrieving the last run time: {str(e)}")
-    return last_run_time
+        return datetime.fromisoformat(last_run_time)
+    except s3_client.exceptions.NoSuchKey:
+        return datetime.min.replace(tzinfo=timezone.utc)
