b8e9cb794d98a937d9305593fb582cf494944b49
ikura1 <ikura.lab@gmail.com>
2024-06-01 00:18:40 +0900

    test: slackへのpostをsnapshotに変更

--- tests/test_post_to_slack.py
+++ tests/test_post_to_slack.py
@@ -1,4 +1,5 @@
 import responses
+from inline_snapshot import snapshot
 
 from FeedSummarizer.lambda_function import post_to_slack
 
@@ -21,5 +22,7 @@ def test_post_to_slack():
 
     # 結果の検証
     assert response.status_code == 200
-    assert responses.calls[0].request.body == b'{"text": "Hello, world!"}'
+    assert responses.calls[0].request.body == snapshot(
+        b'{"attachments": [{"mrkdwn_in": ["text"], "color": "#36a64f", "pretext": "\\u3042\\u3068\\u3067\\u3088\\u3080", "title": "\\u30bf\\u30a4\\u30c8\\u30eb", "title_link": "https://example.com", "image_url": "https://example.com/image.jpg", "fields": [{"title": "\\u8981\\u7d04", "value": "```\\nHello, world!\\n```", "short": false}], "thumb_url": "https://example.com/image.jpg"}]}'
+    )
     assert len(responses.calls) == 1, "Should have made one HTTP call to Slack"
